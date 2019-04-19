95bc5a753bc1eb377dd302fe69a1fe2c62bd9c31
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-04-18 22:44:06 -0700

    Add login and register functionality

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
 
-const API_PORT = 3001;
+
+const User = require('./models/User');
+const UserSession = require('./models/UserSession');
+
+const API_PORT = 3002;
 const app = express();
 app.use(cors());
 const router = express.Router();
@@ -81,6 +85,156 @@ router.post("/putData", (req, res) => {
     return res.json({ success: true });
   });
 });
+
+ /*
+   * Sign up
+   */
+  app.post('/api/account/signup', (req, res, next) => {
+    const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+    const {
+      name
+    } = body;
+    const {
+      confirm_password
+    } = body;
+    let {
+      role
+    } = body;
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+    email = email.trim();
+
+    // Steps:
+    // 1. Verify email doesn't exist
+    // 2. Save
+    User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }
+
+      // Save the new user
+      const newUser = new User();
+
+      newUser.email = email;
+      newUser.name = name;
+      newUser.role = role;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
+
+  });
+
+  app.post('/api/account/signin', (req, res, next) => {
+    const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+
+
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+    email = email.trim();
+
+    User.find({
+      email: email
+    }, (err, users) => {
+      if (err) {
+        console.log('err 2:', err);
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+
+      const user = users[0];
+      if (!user.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+
+      // Otherwise correct user
+      const userSession = new UserSession();
+      userSession.userId = user._id;
+      userSession.save((err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.send({
+            success: false,
+            message: 'Error: server error'
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: 'Valid sign in',
+          token: doc._id
+        });
+      });
+    });
+  });
 
 // append /api for our http requests
 app.use("/api", router);
